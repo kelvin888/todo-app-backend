d8e83500ba78ced9cb6869ee1342c842a5bd2b31
Orhungul Kelvin <kelvin.orhungul@africaprudential.com>
2020-03-31 13:46:58 +0100

    initial spring boot project

---
+++ src/main/java/com/kelvin/todo/TodoAppFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelvin/todo/TodoAppFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
